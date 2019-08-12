84a2c7ed1dd33150b895f6d4240ee79520da6ba9
Jose Almando <josea.dominique01@gmial.com>
2019-08-12 19:58:13 -0400

    Agregue unda indicacion

--- index.py
+++ index.py
@@ -31,3 +31,5 @@ def dft(dft):
 if __name__ == "__main__":
     app.run(debug=True, port=5000) #!debug, para detectar cambios y reiniciar server
                         #port, para el puerto
+
+#No debo hacer nada despues de esto
